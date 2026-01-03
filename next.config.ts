e798640d3b4c83d124b3d0d7d44f420edf289c9c
Adhayan Sharma <96379455+adhayan002@users.noreply.github.com>
2026-01-03 16:25:36 +0530

    next config ts update

--- next.config.ts
+++ next.config.ts
@@ -1,47 +1,10 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   allowedDevOrigins: ['http://192.168.1.5:3000'],
-// };
-
-// module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */
-
-// const nextConfig = {
-//   output: "export",
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
- 
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "export",
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
